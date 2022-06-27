36a5ec6ab9c5f4461e7d622d0f24c0791f14328f
birdlaketree <birdlaketree@gmail.com>
2022-06-27 14:07:11 +0200

    call function in an interval

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
+var numeral = require('numeral');
+
 const app = () => {
-  console.log('hey');
-  const i = 1;
-  console.log('i', i);
+  console.log('hey app');
+  setInterval(checkPrices, 1000);
+}
+
+const checkPrices = () => {
+  console.log('check prices');
 }
 
 const init = app();
